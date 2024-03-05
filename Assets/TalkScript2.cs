8d0a618a65048bfa6cb703ecdb5880e3e3cf89e6
Retr0413 <153156670+Retr0413@users.noreply.github.com>
2024-03-05 21:57:24 +0900

    2024/03/05

--- Assets/TalkScript2.cs
+++ Assets/TalkScript2.cs
@@ -11,7 +11,7 @@ public class TalkScript2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        words = "・,p,i,n,k,の,矢,印,は,押,し,て,O,K,、,\n,し,か,し,N,o,A,r,r,o,w,は,押,し,た,ら,ダ,メ,ー,ジ,を,受,け,ち,ゃ,う,よ,。";
+        words = "・,p,i,n,k,の,矢,印,は,押,し,て,O,K,、,\n,だ,け,ど,N,o,A,r,r,o,w,は,押,し,た,ら,ダ,メ,ー,ジ,を,受,け,ち,ゃ,う,よ,。,\n,気,を,付,け,て,ね,。";
     }
 
     // Update is called once per frame
