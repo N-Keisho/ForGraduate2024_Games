95903e7b89d3491b854bb736613b587d630b0112
Ebisyu-mai <128232415+Ebisyu-mai@users.noreply.github.com>
2024-03-06 01:50:52 +0900

    MusicScore Sabi

--- Assets/KeichanGame/Scripts_keicange/GameController.cs
+++ Assets/KeichanGame/Scripts_keicange/GameController.cs
@@ -25,11 +25,6 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            startMusic = true;
-        }
-
         if (startMusic && musicGo)
         {
             musicGo = false;
