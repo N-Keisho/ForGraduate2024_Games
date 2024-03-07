75a8135297411e86b0bb9afb28c39e1a8da10779
KEI$HO <133760530+N-Keisho@users.noreply.github.com>
2024-03-07 18:52:44 +0900

    とりあえず完成？

--- Assets/RonGame/Script/RonGameManager.cs
+++ Assets/RonGame/Script/RonGameManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class RonGameManager : MonoBehaviour
 {
@@ -14,11 +16,33 @@ public class RonGameManager : MonoBehaviour
     private CharaAnimation[] charaAnimations = new CharaAnimation[6];
     private GameObject[] cameras = new GameObject[6];
     public bool[] questionEnd = new bool[7] { false, false, false, false, false, false, false };
+    private int[][] answerer = new int[6][]{
+        new int[]{0, 3},        // りりーさん
+        new int[]{1, 2, 4},     // カントリーマアム
+        new int[]{3, 2},        // パパパパパイン
+        new int[]{1, 0},     // メンター数
+        new int[]{4, 2, 3},        // 入社順
+        new int[]{0, 1, 4}      // 入社日
+    };
+
+    private string[] answererName = new string[]{
+        "ひろっぺ", "しば", "けいちゃん", "つっくん", "わったー"
+    };
+
+    [SerializeField] private TMP_Text[] ansewererText = new TMP_Text[6];
+
+    [SerializeField] private GameObject QuestionNumberText;
+    private TMP_Text questionNumberText;
+
+    private bool death = false;
+    [SerializeField] private float speed = 1;
 
     void Start()
     {
         questionManager = _QuestionManager_.GetComponent<QuestionManager>();
         hPManager = _HPManager_.GetComponent<HPManager>();
+        questionNumberText = QuestionNumberText.GetComponent<TMP_Text>();
+        QuestionNumberText.SetActive(false);
 
         for (int i = 0; i < characters.Length; i++)
         {
@@ -28,6 +52,16 @@ public class RonGameManager : MonoBehaviour
                 cameras[i] = characters[i].transform.Find("Camera").gameObject;
         }
 
+        for (int i = 0; i < ansewererText.Length; i++)
+        {
+            string text = "";
+            for (int j = 0; j < answerer[i].Length; j++)
+            { 
+                text += answererName[answerer[i][j]] + "\n";
+            }
+            ansewererText[i].text = text;
+        }
+
         StartCoroutine(Play());
 
     }
@@ -35,35 +69,63 @@ public class RonGameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // if(Input.GetKeyDown(KeyCode.D)){
-        //     StartCoroutine(RonQuestion());
-        // }
-        // if(Input.GetKeyDown(KeyCode.Alpha0)){
-        //     StartCoroutine(RonAttack(0));
-        // }
-        // if(Input.GetKeyDown(KeyCode.Alpha1)){
-        //     StartCoroutine(CharaAttack(1));
-        // }
-        // if(Input.GetKeyDown(KeyCode.Alpha2)){
-        //     StartCoroutine(CharaAttack(2));
-        // }
-        // if(Input.GetKeyDown(KeyCode.Alpha3)){
-        //     StartCoroutine(CharaAttack(3));
-        // }
-        // if(Input.GetKeyDown(KeyCode.Alpha4)){
-        //     StartCoroutine(CharaAttack(4));
-        // }
+        if(death){
+            if(characters[5].transform.rotation.y > 0){
+                characters[5].transform.Rotate(new Vector3(-1 * speed * Time.deltaTime, 0, 0));
+            }
+            else{
+                SceneManager.LoadScene("Ending");
+                // return;
+            }
+                
+        }
+        if(Input.GetKeyDown(KeyCode.T)) death = true;
     }
 
     IEnumerator Play()
     {
         for (int i = 0; i < 7; i++)
         {
-            Debug.Log(i);
+            // Debug.Log(i);
             yield return new WaitUntil(() => questionEnd[i]);
-            yield return new WaitForSeconds(2.0f);
-            StartCoroutine(RonQuestion());
+            yield return new WaitForSeconds(1.0f);
+
+            if (i != 0)
+            {
+                if (questionManager.correct[i - 1])
+                {
+                    // Debug.Log(answerer[i-1].Length);
+                    for (int j = 0; j < answerer[i - 1].Length; j++)
+                    {
+                        StartCoroutine(CharaAttack(answerer[i - 1][j]));
+                        yield return new WaitForSeconds(2.5f);
+                    }
+                }
+                else
+                {
+                    StartCoroutine(RonAttack(answerer[i - 1]));
+                    yield return new WaitForSeconds(2f);
+                }
+            }
+            yield return new WaitForSeconds(0.1f);
+
+            if(i != 6){
+                questionNumberText.text = "問" + (i+1);
+                QuestionNumberText.SetActive(true);
+                yield return new WaitForSeconds(2.0f);
+                QuestionNumberText.SetActive(false);
+            
+                StartCoroutine(RonQuestion());
+            }
+            
         }
+
+        Fin();
+    }
+
+    void Fin(){
+        animators[5].SetBool("Damaged", true);
+        death = true;
     }
 
     IEnumerator RonQuestion()
@@ -92,7 +154,7 @@ public class RonGameManager : MonoBehaviour
         animators[5].SetBool("Damaged", false);
     }
 
-    IEnumerator RonAttack(int number)
+    IEnumerator RonAttack(int[] number)
     {
         animators[5].SetBool("Attack", true);
         // animators[number].SetBool("Damaged", true);
@@ -101,7 +163,10 @@ public class RonGameManager : MonoBehaviour
         animators[5].SetBool("Attack", false);
         for (int i = 0; i < 25; i++)
         {
-            hPManager.HPs[number]--;
+            for (int j = 0; j < number.Length; j++)
+            {
+                hPManager.HPs[number[j]]--;
+            }
             yield return new WaitForSeconds(0.01f);
         }
         yield return new WaitForSeconds(0.1f);
