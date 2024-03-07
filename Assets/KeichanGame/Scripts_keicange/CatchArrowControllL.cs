72f65feeb398d6433dfb1f1530f2e37d6c8f8233
Ebisyu-mai <128232415+Ebisyu-mai@users.noreply.github.com>
2024-03-07 18:49:16 +0900

    Joystick

---
+++ Assets/KeichanGame/Scripts_keicange/CatchArrowControllL.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatchArrowControllL : MonoBehaviour
+{
+    SpriteRenderer sprite;
+    public Sprite low;
+    public Sprite high;
+    void Start()
+    {
+        sprite= GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Joystick1Button2))
+        {
+            //Debug.Log(key);
+            sprite.sprite = high;
+            StartCoroutine("ColorBack");
+        }
+    }
+
+    IEnumerator ColorBack()
+    {
+        yield return new WaitForSeconds(0.2f);
+        sprite.sprite = low;
+    }
+}
